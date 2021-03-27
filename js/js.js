5318e7c8c0aab54dd400c91b6c414798afd927fa
MakarevichAV <makarevich.a.v@yandex.ru>
2021-03-27 12:11:13 +0300

    showing menu after click on the button

--- js/js.js
+++ js/js.js
@@ -1,3 +1,4 @@
+// Выплывание header при скролле страницы
 let headerHeight = document.querySelector('header').clientHeight;
 let headerPicture = document.querySelector('.info').clientHeight;
 let header = document.querySelector('.fixed-header');
@@ -8,4 +9,13 @@ window.addEventListener('scroll', () => {
     } else {
         header.classList.remove('drop-down');
     }
+});
+
+// Выплывание мобильного меню
+let menuButton = document.querySelectorAll('.header__menu-button');
+let mobileMenu = document.querySelector('.header__menu-mobile');
+menuButton.forEach((el, ind) => {
+    el.addEventListener('click', () => {
+        mobileMenu.classList.toggle('show-menu');
+    });
 });
