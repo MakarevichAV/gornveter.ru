8dae76282520b1b172a1561019089871292df0c6
MakarevichAV <makarevich.a.v@yandex.ru>
2021-03-27 13:42:26 +0300

    animate page scroll and removing of mobile menu

--- js/js.js
+++ js/js.js
@@ -19,3 +19,25 @@ menuButton.forEach((el, ind) => {
         mobileMenu.classList.toggle('show-menu');
     });
 });
+
+// Исчезновение мобильного меню при клике на пункты меню
+let menuItem = document.querySelectorAll('.header__menu-item a');
+menuItem.forEach((el, ind) => {
+    el.addEventListener('click', () => {
+        mobileMenu.classList.remove('show-menu');
+    });
+});
+
+// Переход к секциям лендинга при клике по пунктам меню
+let menuItemRef = document.querySelectorAll('.local-menu a');
+menuItemRef.forEach((el, ind) => {
+    el.addEventListener('click', (e) => {
+        e.preventDefault();
+        let id = el.getAttribute('href');
+        let targetElement = document.querySelector(id);
+        window.scrollTo({
+            top: targetElement.offsetTop - 100,
+            behavior: 'smooth',
+          });
+    });
+});
