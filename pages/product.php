f7f2d49af9d3f411236589b6b80196f558155d29
MakarevichAV <makarevich.a.v@yandex.ru>
2021-04-06 14:14:40 +0300

    save

---
+++ pages/product.php
@@ -0,0 +1,276 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=0.8">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../styles/style.css">
+    <link rel="stylesheet" href="../styles/logo.css">
+    <link rel="stylesheet" href="../styles/mobile.css">
+    <link rel="stylesheet" href="../styles/product.css">
+    <title>Продукт</title>
+</head>
+
+<body id="top">
+
+    <div class="header__menu-mobile">
+        <ul>
+            <li class="header__menu-item"><a href="../index.html">Главная</a></li>
+            <li class="header__menu-item drop-down-container header__menu-item--active">
+                Наши услуги
+                <span class="menu-item__arrow">&#9660;</span>
+                <ul class="drop-down-block">
+                    <li class="drop-down-block__item"><a href="../pages/service.php?service_name=instalation">Монтаж
+                            сплит-ситем</a></li>
+                    <li class="drop-down-block__item"><a href="../pages/service.php?service_name=laying">Закладка
+                            коммуникаций</a></li>
+                    <li class="drop-down-block__item"><a href="../pages/service.php?service_name=cleaning">Чистка
+                            и
+                            обслуживание кондиционеров</a></li>
+                    <li class="drop-down-block__item"><a href="../pages/service.php?service_name=repair">Ремонт
+                            сплит-систем</a></li>
+                </ul>
+            </li>
+            <li class="header__menu-item"><a href="../index.html#goods">Продукция</a></li>
+            <li class="header__menu-item"><a href="../index.html#about">О нас</a></li>
+            <li class="header__menu-item"><a href="../pages/price.html">Цены</a></li>
+            <li class="header__menu-item"><a href="../index.html#contact">Контакты</a></li>
+        </ul>
+        <div class="info-footer__contact contact">
+            <div class="contact__item">
+                <div class="contact__picture img-phone section__img-phone"></div>
+                <div class="contact__numbers section__numbers">
+                    <a href="tel:+79082548861">+7 908 254-88-61</a>
+                    <a href="tel:+79082548861">+7 908 254-88-61</a>
+                </div>
+            </div>
+            <div class="contact__item">
+                <div class="contact__picture img-email section__img-email"></div>
+                <div class="contact__email section__email">
+                    <a href="email:gornveter@mail.ru">gornveter@mail.ru</a>
+                </div>
+            </div>
+        </div>
+        <button class="btn btn-orange info__btn btn__popup btn__popup--3">КОНСУЛЬТАЦИЯ</button>
+    </div>
+
+    <!-- Header который выплывает при прокрутке  НАЧАЛО -->
+    <div class="fixed-header">
+        <div class="wrap wrap-flex">
+            <div class="header__logo logo">
+
+                <div class="el8"></div>
+                <div class="el1"></div>
+                <div class="el2"></div>
+                <div class="el3"></div>
+                <div class="el4"></div>
+                <div class="el5"></div>
+                <div class="el6"></div>
+                <div class="el7"></div>
+                <div class="el9"></div>
+
+            </div>
+            <div class="header__menu">
+                <ul>
+                    <li class="header__menu-item"><a href="../index.html">Главная</a></li>
+                    <li class="header__menu-item drop-down-container header__menu-item--active">
+                        Наши услуги
+                        <span class="menu-item__arrow">&#9660;</span>
+                        <ul class="drop-down-block">
+                            <li class="drop-down-block__item"><a href="../pages/service.php?service_name=instalation">Монтаж
+                                    сплит-ситем</a></li>
+                            <li class="drop-down-block__item"><a href="../pages/service.php?service_name=laying">Закладка
+                                    коммуникаций</a></li>
+                            <li class="drop-down-block__item"><a href="../pages/service.php?service_name=cleaning">Чистка
+                                    и
+                                    обслуживание кондиционеров</a></li>
+                            <li class="drop-down-block__item"><a href="../pages/service.php?service_name=repair">Ремонт
+                                    сплит-систем</a></li>
+                        </ul>
+                    </li>
+                    <li class="header__menu-item"><a href="../index.html#goods">Продукция</a></li>
+                    <li class="header__menu-item"><a href="../index.html#about">О нас</a></li>
+                    <li class="header__menu-item"><a href="../pages/price.html">Цены</a></li>
+                    <li class="header__menu-item"><a href="../index.html#contact">Контакты</a></li>
+                </ul>
+            </div>
+            <div class="header__menu-button">
+                <div class="line"></div>
+                <div class="line"></div>
+                <div class="line"></div>
+            </div>
+        </div>
+    </div>
+    <!-- Header который выплывает при прокрутке  КОНЕЦ -->
+
+    <header>
+        <div class="wrap wrap-flex">
+            <div class="header__logo logo">
+
+                <div class="el8"></div>
+                <div class="el1"></div>
+                <div class="el2"></div>
+                <div class="el3"></div>
+                <div class="el4"></div>
+                <div class="el5"></div>
+                <div class="el6"></div>
+                <div class="el7"></div>
+                <div class="el9"></div>
+
+            </div>
+            <div class="header__menu">
+                <ul>
+                    <li class="header__menu-item"><a href="../index.html">Главная</a></li>
+                    <li class="header__menu-item drop-down-container header__menu-item--active">
+                        Наши услуги
+                        <span class="menu-item__arrow">&#9660;</span>
+                        <ul class="drop-down-block">
+                            <li class="drop-down-block__item"><a href="../pages/service.php?service_name=instalation">Монтаж
+                                    сплит-ситем</a></li>
+                            <li class="drop-down-block__item"><a href="../pages/service.php?service_name=laying">Закладка
+                                    коммуникаций</a></li>
+                            <li class="drop-down-block__item"><a href="../pages/service.php?service_name=cleaning">Чистка
+                                    и
+                                    обслуживание кондиционеров</a></li>
+                            <li class="drop-down-block__item"><a href="../pages/service.php?service_name=repair">Ремонт
+                                    сплит-систем</a></li>
+                        </ul>
+                    </li>
+                    <li class="header__menu-item"><a href="../index.html#goods">Продукция</a></li>
+                    <li class="header__menu-item"><a href="../index.html#about">О нас</a></li>
+                    <li class="header__menu-item"><a href="../pages/price.html">Цены</a></li>
+                    <li class="header__menu-item"><a href="../index.html#contact">Контакты</a></li>
+                </ul>
+            </div>
+            <div class="header__menu-button">
+                <div class="line"></div>
+                <div class="line"></div>
+                <div class="line"></div>
+            </div>
+        </div>
+    </header>
+    <!-- Повторяющаяся шапка для PHP ---- КОНЕЦ -->
+
+
+    <div class="title">
+        <div class="cover"></div>
+        <h1 class="info__head">Системы {system_name} </h1>
+    </div>
+
+    <div class="section section-type1">
+        <div class="product-card">
+            <div class="product-card__picture"></div>
+            <div class="product-card__info">
+                <h4>Система {system_name}</h4>
+                <select name="model" id="model" class="btn select">
+                    <option value="model_number">Модель {model_number}</option>
+                    <option value="model_number">Модель {model_number}</option>
+                    <option value="model_number">Модель {model_number}</option>
+                    <option value="model_number">Модель {model_number}</option>
+                    <option value="model_number">Модель {model_number}</option>
+                    <option value="model_number">Модель {model_number}</option>
+                </select>
+                <p class="price">25 000 ₽</p>
+                <button class="btn btn-orange">Проконсультироваться</button>
+                <button class="btn btn-blue">Расчитать стоимость</button>
+            </div>
+        </div>
+    </div>
+    <div class="section section-type2">
+        <div class="product-info">
+            <ul class="product-info__nav">
+                <li>
+                    Описание
+                </li>
+                <li>
+                    Характеристики
+                </li>
+            </ul>
+            <div class="product-info__content"></div>
+        </div>
+        <form class="feetback__flex-container">
+            <input class="feetback__input btn" type="tel" maxlength="50" id="online_phone" name="phone" required="required" pattern="\+7\s?[\(]{0,1}9[0-9]{2}[\)]{0,1}\s?\d{3}[-]{0,1}\d{2}[-]{0,1}\d{2}" placeholder="+7(___)___-__-__" />
+            <button class="btn btn-orange feetback__btn">Проконсультироваться</button>
+        </form>
+    </div>
+
+    <footer class="footer">
+        <div class="footer__top">
+            <div class="footer__flex-container">
+                <div class="footer__menu">
+                    <ul class="footer__menu-block">
+                        <li class="footer__menu-item"><a href="../index.html">Главная</a></li>
+                        <li class="footer__menu-item"><a href="../index.html#goods">Продукция</a></li>
+                        <li class="footer__menu-item"><a href="../index.html#about">О нас</a></li>
+                        <li class="footer__menu-item"><a href="#top">Цены</a></li>
+                        <li class="footer__menu-item"><a href="../index.html#contact">Контакты</a></li>
+                    </ul>
+                    <ul class="footer__menu-block">
+                        <li class="footer__menu-item"><a href="../pages/instalation.php">Монтаж
+                                сплит-ситем</a></li>
+                        <li class="footer__menu-item"><a href="../pages/laying.php">Закладка
+                                коммуникаций</a></li>
+                        <li class="footer__menu-item"><a href="../pages/service.php">Чистка и
+                                обслуживание кондиционеров</a></li>
+                        <li class="footer__menu-item"><a href="../pages/repair.php">Ремонт
+                                сплит-систем</a></li>
+                    </ul>
+                </div>
+                <div class="footer__logo">
+                    <div class="logo">
+                        <div class="el8"></div>
+                        <div class="el1"></div>
+                        <div class="el2"></div>
+                        <div class="el3"></div>
+                        <div class="el4"></div>
+                        <div class="el5"></div>
+                        <div class="el6"></div>
+                        <div class="el7"></div>
+                        <div class="el9"></div>
+                    </div>
+                    <div class="social">
+                        <a href="vk.com" class="social__icon" style="background:
+                                url(../images/icon/vk.svg) no-repeat center
+                                center /
+                                contain;"></a>
+                        <a href="instagram.com" class="social__icon" style="background: url(../images/icon/insta.svg)
+                                no-repeat center center / contain;"></a>
+                        <a href="https://tlgg.ru/@dunkan89" class="social__icon" style="background: url(../images/icon/tg.svg)
+                                no-repeat center center / contain;"></a>
+                        <a href="https://wa.me/79082548861?text=%D0%97%D0%B4%D1%80%D0%B0%D0%B2%D1%81%D1%82%D0%B2%D1%83%D0%B9%D1%82%D0%B5!%20%D0%AF%20%D0%B1%D1%8B%20%D1%85%D0%BE%D1%82%D0%B5%D0%BB%20%D0%BF%D1%80%D0%BE%D0%BA%D0%BE%D0%BD%D1%81%D1%83%D0%BB%D1%8C%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D1%82%D1%8C%D1%81%D1%8F" class="social__icon" style="background: url(../images/icon/wp.svg)
+                                no-repeat center center / contain;"></a>
+                        <a href="https://viber.click/79082548861" class="social__icon" style="background: url(../images/icon/vb.svg)
+                                no-repeat center center / contain;"></a>
+                    </div>
+                </div>
+
+                <div class="info-footer__contact contact">
+                    <div class="contact__item">
+                        <div class="contact__picture img-phone"></div>
+                        <div class="contact__numbers">
+                            <a href="tel:+79082548861">+7 908 254-88-61</a>
+                            <a href="tel:+79082548861">+7 908 254-88-61</a>
+                        </div>
+                    </div>
+                    <div class="contact__item">
+                        <div class="contact__picture img-email"></div>
+                        <div class="contact__email">
+                            <a href="email:gornveter@mail.ru">gornveter@mail.ru</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer__bottom">
+            <p>© 2018 - 2021 "gornveter.ru". Все права защищены</p>
+            <p>Сайт разработан веб-студией <a href="https://vk.com/makarchik89">Makar&M</a> </p>
+        </div>
+    </footer>
+
+    <script src="./js/js.js"></script>
+
+</body>
+
+</html>
