8a2f42a2ab37e75dfc275a89354c2f2c96ff803d
MakarevichAV <makarevich.a.v@yandex.ru>
2021-04-12 16:20:41 +0300

    save

--- control/product_adding.php
+++ control/product_adding.php
@@ -31,18 +31,19 @@
                     <li><a href="./product_editing.php">Изменить модель</a></li>
                 </ul>
                 <h1>Заполнение карточки продукта</h1>
-                <select name="system_name">
-                    <option value="{system_name}">{system_name}</option>
-                    <option value="{system_name}">{system_name}</option>
-                    <option value="{system_name}">{system_name}</option>
-                    <option value="{system_name}">{system_name}</option>
-                </select>
-                <input type="text" name="model" placeholder="Введите название модели" />
-                <input type="number" name="price" placeholder="Введите цену" />
-                <br />
-                <h2>Описание модели</h2>
-                <p>Для переноса строки введите &lt;br /&gt; </p>
-                <textarea name="description" placeholder="Введите описание модели">Пример:
+                <form action="">
+                    <select name="system_name">
+                        <option value="{system_name}">{system_name}</option>
+                        <option value="{system_name}">{system_name}</option>
+                        <option value="{system_name}">{system_name}</option>
+                        <option value="{system_name}">{system_name}</option>
+                    </select>
+                    <input type="text" name="model" placeholder="Введите название модели" />
+                    <input type="number" name="price" placeholder="Введите цену" />
+                    <br />
+                    <h2>Описание модели</h2>
+                    <p>Для переноса строки введите &lt;br /&gt; </p>
+                    <textarea name="description" placeholder="Введите описание модели">Пример:
 С новой сплит-системой Perfecto Вы сможете не только охлаждать помещение летом, но и поддерживать необходимую температуру в загородном доме без центрального отопления зимой. Благодаря функции «Защита дома от замерзания» кондиционер будет поддерживать температуру воздуха +8°С, не позволяя заморозить помещение и потребляя минимум электроэнергии. Безупречное качество и надежность работы кондиционера подтверждаются расширенной гарантией 5 лет.
 <br />
 <br />• Класс энергоэффективности "A"
@@ -60,76 +61,78 @@
 <br />• Таймер 24 часа
 <br />• Вывод дренажа в 2 стороны</textarea>
 
-                <br />
-                <h2>Характеристики</h2>
-                <div class="parametr">
-                    <p>Производительность (охлаждение/обогрев)</p>
-                    <input type="text" name="param_1">
-                </div>
-                <div class="parametr">
-                    <p>Класс энергоэффективности</p>
-                    <input type="text" name="param_2">
-                </div>
-                <div class="parametr">
-                    <p>Расход воздуха (внутренний /внешний блок)</p>
-                    <input type="text" name="param_3">
-                </div>
-                <div class="parametr">
-                    <p>Уровень шума внутреннего блока</p>
-                    <input type="text" name="param_4">
-                </div>
-                <div class="parametr">
-                    <p>Напряжение питания</p>
-                    <input type="text" name="param_5">
-                </div>
-                <div class="parametr">
-                    <p>Потребляемая мощность (охлаждение/обогрев)</p>
-                    <input type="text" name="param_6">
-                </div>
-                <div class="parametr">
-                    <p>Номинальный ток (охлаждение/обогрев)</p>
-                    <input type="text" name="param_7">
-                </div>
-                <div class="parametr">
-                    <p>Размеры внутреннего блока (Ш×В×Г)</p>
-                    <input type="text" name="param_8">
-                </div>
-                <div class="parametr">
-                    <p>Размеры внешнего блока (Ш×В×Г)</p>
-                    <input type="text" name="param_9">
-                </div>
-                <div class="parametr">
-                    <p>Вес нетто/брутто внутреннего блока</p>
-                    <input type="text" name="param_10">
-                </div>
-                <div class="parametr">
-                    <p>Вес нетто/брутто внешнего блока</p>
-                    <input type="text" name="param_11">
-                </div>
-                <div class="parametr">
-                    <p>Диаметр труб (жидкость/газ)</p>
-                    <input type="text" name="param_12">
-                </div>
-                <div class="parametr">
-                    <p>Максимальная длина магистрали</p>
-                    <input type="text" name="param_13">
-                </div>
-                <div class="parametr">
-                    <p>Максимальный перепад высот</p>
-                    <input type="text" name="param_14">
-                </div>
-                <div class="parametr">
-                    <p>Хладагент</p>
-                    <input type="text" name="param_15">
-                </div>
-                <div class="parametr">
-                    <p>Диапазон рабочих температур (охлаждение/обогрев)</p>
-                    <input type="text" name="param_16">
-                </div>
-                <div class="parametr">
-                    <p>Марка компрессора</p>
-                    <input type="text" name="param_17">
-                </div>
+                    <br />
+                    <h2>Характеристики</h2>
+                    <div class="parametr">
+                        <p>Производительность (охлаждение/обогрев)</p>
+                        <input type="text" name="param_1">
+                    </div>
+                    <div class="parametr">
+                        <p>Класс энергоэффективности</p>
+                        <input type="text" name="param_2">
+                    </div>
+                    <div class="parametr">
+                        <p>Расход воздуха (внутренний /внешний блок)</p>
+                        <input type="text" name="param_3">
+                    </div>
+                    <div class="parametr">
+                        <p>Уровень шума внутреннего блока</p>
+                        <input type="text" name="param_4">
+                    </div>
+                    <div class="parametr">
+                        <p>Напряжение питания</p>
+                        <input type="text" name="param_5">
+                    </div>
+                    <div class="parametr">
+                        <p>Потребляемая мощность (охлаждение/обогрев)</p>
+                        <input type="text" name="param_6">
+                    </div>
+                    <div class="parametr">
+                        <p>Номинальный ток (охлаждение/обогрев)</p>
+                        <input type="text" name="param_7">
+                    </div>
+                    <div class="parametr">
+                        <p>Размеры внутреннего блока (Ш×В×Г)</p>
+                        <input type="text" name="param_8">
+                    </div>
+                    <div class="parametr">
+                        <p>Размеры внешнего блока (Ш×В×Г)</p>
+                        <input type="text" name="param_9">
+                    </div>
+                    <div class="parametr">
+                        <p>Вес нетто/брутто внутреннего блока</p>
+                        <input type="text" name="param_10">
+                    </div>
+                    <div class="parametr">
+                        <p>Вес нетто/брутто внешнего блока</p>
+                        <input type="text" name="param_11">
+                    </div>
+                    <div class="parametr">
+                        <p>Диаметр труб (жидкость/газ)</p>
+                        <input type="text" name="param_12">
+                    </div>
+                    <div class="parametr">
+                        <p>Максимальная длина магистрали</p>
+                        <input type="text" name="param_13">
+                    </div>
+                    <div class="parametr">
+                        <p>Максимальный перепад высот</p>
+                        <input type="text" name="param_14">
+                    </div>
+                    <div class="parametr">
+                        <p>Хладагент</p>
+                        <input type="text" name="param_15">
+                    </div>
+                    <div class="parametr">
+                        <p>Диапазон рабочих температур (охлаждение/обогрев)</p>
+                        <input type="text" name="param_16">
+                    </div>
+                    <div class="parametr">
+                        <p>Марка компрессора</p>
+                        <input type="text" name="param_17">
+                    </div>
+                    <button type="submit">Сохранить</button>
+                </form>
             </div>
         </div>
     </div>
