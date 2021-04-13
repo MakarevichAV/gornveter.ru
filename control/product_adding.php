e757ac01fdb04b27376fb433b7f4870f84ed3a96
MakarevichAV <makarevich.a.v@yandex.ru>
2021-04-13 22:43:24 +0300

    save

--- control/product_adding.php
+++ control/product_adding.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/product_card.css">
+    <link rel="stylesheet" href="styles/common.css">
     <title>Add Product Info</title>
 </head>
 
